c7ee9098b7c4a46297f3371d8a0837ec5393033d
super-user3653 <codepro365@gmail.com>
2020-10-25 20:50:02 +0500

    finished-flow

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+// cookie session
+const cookieSession = require('cookie-session');
+
+// passport 
+const passport = require('passport');
+
+const keys = require('./config/keys');
+
+// models
+require('./models/User');
+// passport
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
-});
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+// routes
+require('./routes/authRoutes')(app);
 
+// listening-----------------------------------------------------------------------------------------------------------------
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
