c59acf9698fa59b2dce91003c468587326752213
super-user3653 <codepro365@gmail.com>
2020-10-27 09:16:50 +0500

    27-10-20

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+// import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+
+            hello
+            <a href="/auth/google"> sign in</a>
+        </div>
+    );
+}
+
+export default App;
--- services/passport.js
+++ services/passport.js
@@ -23,19 +23,18 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-            if (existingUser) {
-                done(null, existingUser)
-            } else {
-                new User({
-                    googleId: profile.id
-                }).save()
-                    .then(user => done(null, user));
-            }
-        })
+    callbackURL: '/auth/google/callback',
+    proxy: true
+}, async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id })
+
+    if (existingUser) {
+        done(null, existingUser)
+    } else {
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user);
+    }
+
 
 }));
 // passport---------------------------------
